1674fa94cb0071801bc86b18eb48e84cab8cc3b5
Mona <borisagarmona107@gmail.com>
2025-09-12 10:39:14 -0400

    note added and environmentts file build with different configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCeritificationPractice';
+  apiUrl = environment.apiUrl;
+
 }
